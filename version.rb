e7726f11712ab419805403282b7094c78ba9d306
Dana Scheider <dana.scheider@gmail.com>
2015-04-04 13:23:54 -0700

    Fix version - quotes around 'alpha'

--- version.rb
+++ version.rb
@@ -2,7 +2,7 @@ module TOC
   MAJOR  = 0
   MINOR  = 0
   PATCH  = 1
-  SUFFIX = alpha
+  SUFFIX = 'alpha'
 
   def self.gem_version
     SUFFIX ? "#{MAJOR}.#{MINOR}.#{PATCH}.#{SUFFIX}" : "#{MAJOR}.#{MINOR}.#{PATCH}"
