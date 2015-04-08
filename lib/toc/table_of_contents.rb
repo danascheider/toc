2c00b1f89aab077a5d5b0c76d414937f7266664c
Dana Scheider <dana.scheider@gmail.com>
2015-04-08 11:39:50 -0700

    Test the create_line method for the TableOfContents class

--- lib/toc/table_of_contents.rb
+++ lib/toc/table_of_contents.rb
@@ -5,6 +5,17 @@ module TOC
       @ending = File.readlines(File.expand_path('../../formats/ending.txt', __FILE__)).join
     end
 
+    def self.toc_format
+      /^\/(\*{88})\n \*( ){86}\*\n \* CONTENTS( ){71}LINE  \*\n( \* [A-Za-z0-9 ]*\.* [0-9]{1,5} +\*\n)* \*( )+\*\n\/\**\/\n$/
+    end
+
+    # The +create_line+ method takes a section +name+ and line +number+ as arguments. It then
+    # returns a table-of-contents entry 90 characters long, incorporating that section name and
+    # line number and adding the appropriate periods, asterisks, etc.
+    #
+    #     table.create_line 'Core Properties', 120
+    #       # => ' * Core Properties ................................................................. 120 *'
+
     def create_line name, number
       beg    = " * #{name} "
       ending = " #{number}  *"
--- spec/table_of_contents_spec.rb
+++ spec/table_of_contents_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe TOC::TableOfContents do 
   describe 'initialize' do 
     before(:each) { @toc = TOC::TableOfContents.new }
+
     it 'sets the @beginning variable' do 
       beginning = [
         "/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\n", 
@@ -19,6 +20,22 @@ describe TOC::TableOfContents do
         "/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*/\n"
         ].join
       expect(@toc.instance_values['ending']).to eql ending
+    end
+  end
+
+  describe 'create_line' do 
+    before(:each) do 
+      @toc  = TOC::TableOfContents.new
+      @name = 'Core Properties'
+      @num  = 20
+    end
+
+    it 'includes the name at the beginning' do 
+      expect(@toc.create_line @name, @num).to match(/^ \* Core Properties/)
+    end
+
+    it 'includes the number at the end' do 
+      expect(@toc.create_line @name, @num).to match(/ 20  \*( )?$/)
     end
   end
 end
