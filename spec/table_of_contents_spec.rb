5deff7377ba855c993a3e7b79b48a6e9472a0420
Dana Scheider <dana.scheider@gmail.com>
2015-04-08 11:17:53 -0700

    Add another test for the Table of Contents object

--- spec/table_of_contents_spec.rb
+++ spec/table_of_contents_spec.rb
@@ -2,15 +2,23 @@ require 'spec_helper'
 
 describe TOC::TableOfContents do 
   describe 'initialize' do 
+    before(:each) { @toc = TOC::TableOfContents.new }
     it 'sets the @beginning variable' do 
-      beginning = [ 
-        "/*****************************************************************************************", 
-        " *                                                                                       *", 
-        " * CONTENTS                                                                        LINE  *"
-        ].join("\n")
+      beginning = [
+        "/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\n", 
+        " \*                                                                                       \*\n", 
+        " \* CONTENTS                                                                        LINE  \*"
+        ].join
 
-      toc = TOC::TableOfContents.new
-      expect(toc.instance_values['beginning']).to eql beginning
+      expect(@toc.instance_values['beginning']).to eql beginning
+    end
+
+    it 'sets the @ending variable' do 
+      ending = [
+        " \*                                                                                       \*\n", 
+        "/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*/\n"
+        ].join
+      expect(@toc.instance_values['ending']).to eql ending
     end
   end
 end
