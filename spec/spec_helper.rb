f76b78fafa21bd2d0b3e39a609c8255a11dfb9fd
Dana Scheider <dana.scheider@gmail.com>
2015-04-08 13:18:36 -0700

    Restore example1 to its original text after the suite runs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,12 @@
 require 'reactive_support/core_ext/object/instance_variables'
+require 'fileutils'
 
 Dir.glob('./lib/**/*.rb').each {|file| require file }
 Dir.glob('support/**/*.rb').each {|file| require file }
+
+RSpec.configure do |c|
+  c.after(:suite) do 
+    path = File.expand_path('..', __FILE__);
+    FileUtils.cp("#{path}/support/example1.txt", "#{path}/support/example1.js")
+  end
+end
