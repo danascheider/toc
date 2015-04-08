f15aa1afc077a2c9c8d9bddf290b1f4f495279c4
Dana Scheider <dana.scheider@gmail.com>
2015-04-08 13:23:50 -0700

    Restore example1.js to original form between each test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ Dir.glob('./lib/**/*.rb').each {|file| require file }
 Dir.glob('support/**/*.rb').each {|file| require file }
 
 RSpec.configure do |c|
-  c.after(:suite) do 
+  c.after(:each) do 
     path = File.expand_path('..', __FILE__);
     FileUtils.cp("#{path}/support/example1.txt", "#{path}/support/example1.js")
   end
